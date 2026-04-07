cf9c7df9d520c62e1c8f8b9354aab4675ab8d93e
Hunde Tolera <hunda4863@gmail.com>
2026-04-07 17:41:13 -0400

    Update __init__.py

--- Statical_engine/src/__init__.py
+++ Statical_engine/src/__init__.py
@@ -1,9 +1,4 @@
 # __int__ difinition 
 
-def __init__(self, data: List[Union[Number, str, None]]):
-        self.raw_data = data
-        self.data = self._clean_data(data)
-  
-  def __init__(self, crash_prob=0.045, seed=42):
-        self.crash_prob = crash_prob
-        random.seed(seed)
+# stat_engine/__init__.py
+from .core import StatEngine as Engine

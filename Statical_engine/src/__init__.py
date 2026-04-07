4e25c2cfeda1556ec9688a10a183ae74a0c5959b
Hunde Tolera <hunda4863@gmail.com>
2026-04-07 16:00:39 -0400

    Update __init__.py

--- Statical_engine/src/__init__.py
+++ Statical_engine/src/__init__.py
@@ -1,1 +1,9 @@
+# __int__ difinition 
 
+def __init__(self, data: List[Union[Number, str, None]]):
+        self.raw_data = data
+        self.data = self._clean_data(data)
+  
+  def __init__(self, crash_prob=0.045, seed=42):
+        self.crash_prob = crash_prob
+        random.seed(seed)
